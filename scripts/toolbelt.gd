dca4b19fa4468b3c4f650635d062b64464500ba1
PatrickQ <Patrick@Quiskamp.de>
2025-10-04 15:48:30 +0200

    Fix

--- scripts/toolbelt.gd
+++ scripts/toolbelt.gd
@@ -17,3 +17,8 @@ func change_tool(newTool:Tools):
 	currentTool = newTool
 	print(Tools.keys()[currentTool], " Tool selected!")
 	tool_has_changed.emit(currentTool)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+			change_tool(Tools.Hand)
