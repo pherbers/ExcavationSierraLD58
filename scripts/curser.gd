93ef59c6dacdd68a4f408ff9e8496b04af41ac94
PatrickQ <Patrick@Quiskamp.de>
2025-10-04 13:19:10 +0200

    Curser

--- scripts/curser.gd
+++ scripts/curser.gd
@@ -1,6 +1,37 @@
 extends Node2D
 class_name Curser
 
+@export var curserAnimator: AnimatedSprite2D
+
+@export var toolbelt: Toolbelt
+
+var lastTool: Toolbelt.Tools = Toolbelt.Tools.Shovel
+
+func _ready() -> void:
+	changeToShovel()
+
 func _process(_delta: float):
 	var posMouse = get_global_mouse_position()
-	print(posMouse)
+	position = posMouse
+	
+	var currentTool = toolbelt.currentTool;
+	if currentTool != lastTool:
+		lastTool = currentTool
+		if currentTool == Toolbelt.Tools.Shovel:
+			changeToShovel()
+		elif currentTool == Toolbelt.Tools.Trowel:
+			changeToTrowel()
+		elif currentTool == Toolbelt.Tools.Brush:
+			changeToBrush()
+
+func changeToShovel():
+	curserAnimator.stop()
+	curserAnimator.play("Shovel")
+	
+func changeToTrowel():
+	curserAnimator.stop()
+	curserAnimator.play("Shovel")
+	
+func changeToBrush():
+	curserAnimator.stop()
+	curserAnimator.play("Shovel")
