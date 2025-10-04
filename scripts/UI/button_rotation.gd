56143a40b341227edc3cd359d9381eed8f761653
PatrickQ <Patrick@Quiskamp.de>
2025-10-04 17:39:03 +0200

    Rotation

---
+++ scripts/UI/button_rotation.gd
@@ -0,0 +1,6 @@
+extends Button
+
+@export var tollbelt: Toolbelt
+
+func _on_pressed() -> void:
+	tollbelt.change_direction_up()
--- scripts/toolbelt.gd
+++ scripts/toolbelt.gd
@@ -5,7 +5,7 @@ signal tool_has_changed(newTool:Tools)
 
 @export var digSite: DigSite
 
-var isAktive: bool = false
+var direction: int = 0;
 
 enum Tools {
 	Hand,
@@ -14,13 +14,32 @@ enum Tools {
 	Brush
 }
 
+var tool_dic_mod = {
+	Tools.Hand : 8,
+	Tools.Shovel : 4,
+	Tools.Trowel : 8,
+	Tools.Brush : 8
+}
+
 static var currentTool: Tools = Tools.Hand
 
 func change_tool(newTool:Tools):
 	if currentTool == newTool: return
 	currentTool = newTool
+	direction = 0
 	print(Tools.keys()[currentTool], " Tool selected!")
 	tool_has_changed.emit(currentTool)
+
+func change_direction_up():
+	direction += 1
+	direction = direction % tool_dic_mod[currentTool]
+	print("New Direction:", direction)
+	
+func change_direction_down():
+	direction -= 1
+	if direction < 0: 
+		direction = tool_dic_mod[currentTool] - 1
+	print("New Direction:", direction)
 
 func digSiteAktion(pressed:bool):
 	if !pressed:
@@ -30,9 +49,9 @@ func digSiteAktion(pressed:bool):
 			Tools.Hand:
 				digSite.take_object(digSite.getTileForMousePos())
 			Tools.Shovel: 
-				digSite.dig_shovel(digSite.getTileForMousePos(), 0)
+				digSite.dig_shovel(digSite.getTileForMousePos(), direction)
 			Tools.Trowel:
-				digSite.dig_trowel(digSite.getTileForMousePos(), 0)
+				digSite.dig_trowel(digSite.getTileForMousePos(), direction)
 			Tools.Brush:
 				digSite.is_brushing = true
 
@@ -44,4 +63,8 @@ func _input(event: InputEvent) -> void:
 			digSiteAktion(true)
 		elif !event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 			digSiteAktion(false)
+		elif event.pressed and event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			change_direction_up()
+		elif event.pressed and event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			change_direction_down()
 		
