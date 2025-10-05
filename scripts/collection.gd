cf88b5c944ce1ac053f950bd033b1cb251c22b3f
PatrickQ <Patrick@Quiskamp.de>
2025-10-05 11:20:53 +0200

    Collection with Bones

--- scripts/collection.gd
+++ scripts/collection.gd
@@ -7,33 +7,70 @@ class_name Collection
 @export var collection: Node
 @export var prefab: PackedScene
 
+@export var baseWorkArea: TileMapLayer
+
 var aktiveBones = {}
 
 var selectedBone: CollectionBone = null
 
 func _ready() -> void:
-	for i in range(20):
-		var newBode: CollectionBone = prefab.instantiate()
-		newBode.position = spawnPoint.position
-		newBode.collection = self
-		collection.add_child(newBode)
+	createBone("bone_d_1_b_5")
 		
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT and toolbelt.currentTool == Toolbelt.Tools.Hand:
 			performAktion()
+	if selectedBone != null and toolbelt.currentTool != Toolbelt.Tools.Hand:
+		resetSelectedBone()
+		dropBone()
 
 func _process(_delta: float):
 	if selectedBone != null:
 		var posMouse = get_global_mouse_position()
-		selectedBone.position = posMouse
+		selectedBone.global_position = posMouse
+	
+		var boundingBox = baseWorkArea.get_used_rect();
+		var globalStart = baseWorkArea.to_global(baseWorkArea.map_to_local(boundingBox.position))
+		var globalSize = baseWorkArea.map_to_local(boundingBox.size)
+		var rect: Rect2 = Rect2(globalStart, globalSize)
+	
+		if !rect.has_point(posMouse):
+			resetSelectedBone()
+			dropBone()
+		
+	
+
+func createBone(nameOfBone: String):
+	var newBode: CollectionBone = prefab.instantiate()
+	newBode.position = spawnPoint.position
+	newBode.collection = self
+	collection.add_child(newBode)
+	
+	var loaded: PackedScene = load("res://nodes/bones/" + nameOfBone + ".tscn")
+	var nodeTile: TileMapLayer = loaded.instantiate()
+	nodeTile.scale = Vector2(5, 5)
+	newBode.add_child(nodeTile)
+	
+	var boundingRectI = nodeTile.get_used_rect()
+	var globalStart = nodeTile.to_global(nodeTile.map_to_local(boundingRectI.position))
+	var globalEnd = nodeTile.to_global(nodeTile.map_to_local(boundingRectI.end))
+	
+	var diff = globalEnd - globalStart
+	
+	newBode.collider.shape.set_size(diff * 6)
+	
+
+func resetSelectedBone():
+	if selectedBone == null:
+		pass
+	else:
+		selectedBone.position = spawnPoint.position
 
 func performAktion(): 
 	if selectedBone == null:
 		pickUpBone()
 	else:
 		dropBone()
-	
 
 func pickUpBone():
 	var canndidate = findBestBone()
