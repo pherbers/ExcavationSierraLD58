147110ac883212e883748a0b753d72406ef0e5fc
PatrickQ <Patrick@Quiskamp.de>
2025-10-05 17:14:12 +0200

    Toggle + >Toolbelt v2

--- scripts/toolbelt.gd
+++ scripts/toolbelt.gd
@@ -5,11 +5,28 @@ signal tool_has_changed(newTool:Tools)
 signal direction_has_changed(newDir:int)
 
 @export var digSite: DigSite
+@export var collectionSpawnPoint: Node2D
 
 @onready var camera = $/root/MainScene/Camera2D as Camera2D
 @onready var gameState = $/root/MainScene/GameState as GameState
 
+@export var toolSprite: Sprite2D
+@export var defaultTexture: Texture2D
+
+@export var shovelHighlightedTexture: Texture2D
+@export var shovelUsedTexture: Texture2D
+
+@export var trowelHighlightedTexture: Texture2D
+@export var trowelUsedTexture: Texture2D
+
+@export var brushHighlightedTexture: Texture2D
+@export var brushUsedTexture: Texture2D
+
+@export var gprHighlightedTexture: Texture2D
+@export var gprUsedTexture: Texture2D
+
 var direction: int = 0;
+var highlightTool: Tools = Tools.Hand
 
 signal shovel_used
 signal trowel_used
@@ -34,7 +51,14 @@ var tool_dic_mod = {
 
 static var currentTool: Tools = Tools.Hand
 
+var isInCollection: bool = false
+var playerPosOnField: Vector2
+var playerPosOnCollection: Vector2
+
 func _ready() -> void:
+    playerPosOnField = camera.global_position
+    playerPosOnCollection = collectionSpawnPoint.global_position
+    
     get_viewport().size_changed.connect(window_update)
     window_update()
 
@@ -44,7 +68,12 @@ func window_update():
     camera.zoom = Vector2i(zoom, zoom)
 
 func change_tool(newTool:Tools):
-    if currentTool == newTool: return
+    if currentTool == newTool:
+        if currentTool == Tools.Hand:
+            return
+        else:
+            change_tool(Tools.Hand)
+            return
     currentTool = newTool
     direction = 0
     tool_has_changed.emit(currentTool)
@@ -57,6 +86,8 @@ func switch_to_brush():
     change_tool(Tools.Brush)
 func switch_to_GPR():
     change_tool(Tools.GPR)
+func switch_to_hand():
+    change_tool(Tools.Hand)
 
 func change_direction_up():
     direction += 1
@@ -114,3 +145,58 @@ func _unhandled_input(event: InputEvent) -> void:
 func _process(_delta: float) -> void:
     var camBottomRight = camera.get_screen_center_position() + Vector2(get_viewport_rect().size / camera.zoom / 2)
     global_position = camBottomRight
+    
+    if isInCollection:
+        playerPosOnCollection = camera.global_position
+    else: 
+        playerPosOnField = camera.global_position
+    
+    if currentTool == Tools.Hand:
+        if highlightTool == Tools.Hand:
+            toolSprite.texture = defaultTexture
+        elif highlightTool == Tools.Shovel:
+            toolSprite.texture = shovelHighlightedTexture
+        elif highlightTool == Tools.Trowel:
+            toolSprite.texture = trowelHighlightedTexture
+        elif highlightTool == Tools.Brush:
+            toolSprite.texture = brushHighlightedTexture
+        elif highlightTool == Tools.GPR:
+            toolSprite.texture = gprHighlightedTexture
+    elif currentTool == Tools.Shovel:
+        toolSprite.texture = shovelUsedTexture
+    elif currentTool == Tools.Trowel:
+        toolSprite.texture = trowelUsedTexture
+    elif currentTool == Tools.Brush:
+        toolSprite.texture = brushUsedTexture
+    elif currentTool == Tools.GPR:
+        toolSprite.texture = gprUsedTexture
+    
+    
+func reset_highlight():
+    highlightTool = Tools.Hand    
+    
+func highlight_shove():
+    highlightTool = Tools.Shovel
+    
+func highlight_brush():
+    highlightTool = Tools.Brush
+    
+func highlight_trowel():
+    highlightTool = Tools.Trowel
+    
+func highlight_gpr():
+    highlightTool = Tools.GPR
+    
+func toggle_collection():
+    if isInCollection:
+        camera.global_position = playerPosOnField
+        isInCollection = false
+    else: 
+        camera.global_position = playerPosOnCollection
+        isInCollection = true
+        
+func forcePlayerToCollectionSpawnPoint():
+    if isInCollection:
+       toggle_collection()
+    playerPosOnCollection = collectionSpawnPoint.global_position
+    toggle_collection()
