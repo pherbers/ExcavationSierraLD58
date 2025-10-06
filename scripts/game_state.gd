ae89bf4e40f3963267c1eb753313ad5ed4fdcc98
Patrick <patrick.herbers@ruhr-uni-bochum.de>
2025-10-06 13:29:02 +0200

    Shop UI

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -14,8 +14,8 @@ var item_has_gpr = true
 var item_gpr_width = 2  # 0, 1, 2
 var item_gpr_depth = 2  # max 6
 
-signal coins_has_changed(newValue:int)
-signal shop_has_changed(shopItems: Array[ShopItem])
+@onready var shop: Shop = $/root/MainScene/Shop as Shop
+
 
 @export var toolbelt: Toolbelt
 
@@ -31,96 +31,8 @@ class BoneDamage:
     func equals(d: BoneDamage) -> bool:
         return d.atlas_id == atlas_id and d.atlas_pos == atlas_pos and d.bone_name == bone_name
 
-var shopItems: Array[ShopItem]
-     
-class ShopItem:
-    var id: int
-    var maxLevel: int
-    var currentLevel: int 
-    var price: Array[int]
-    
-    func isMaxLevel():
-        if self.currentLevel < self.maxLevel:
-            return false
-        return true
-    
-    func current_price():
-        if self.currentLevel < price.size(): 
-            return price[self.currentLevel]
-        else:
-            return -1
-    
-    func updateOne():
-        if !self.isMaxLevel():
-            self.currentLevel += 1
-            return true
-        else:
-            return false
-            
-    func self_print():
-        print(
-            "Name: ", self.tool, " ", self.id,
-            " Level: ", self.currentLevel, "/", self.maxLevel,
-            " Prices: ", self.price,
-            )
-            
-    func can_be_bought(coins:int):
-        var currentPrice = self.current_price()
-        if !self.isMaxLevel() and currentPrice != -1 and currentPrice <= coins:
-            return true
-        return false
-            
-            
-var coins: int
 
 @export var isCollectionComplete = false
-
-func _ready() -> void:
-    coins = 0
-    coins_has_changed.emit()
-    
-    shopItems = []
-    #Shovel 
-    var shopItemShovel: ShopItem = ShopItem.new()
-    shopItemShovel.id = 0;
-    shopItemShovel.maxLevel = 2
-    shopItemShovel.currentLevel = 0
-    shopItemShovel.price = [20, 150]
-    shopItems.append(shopItemShovel)
-
-    #Trowel 
-    var shopItemTrowel: ShopItem = ShopItem.new()
-    shopItemTrowel.id = 1;
-    shopItemTrowel.maxLevel = 2
-    shopItemTrowel.currentLevel = 0
-    shopItemTrowel.price = [25, 300]
-    shopItems.append(shopItemTrowel) 
-     
-    #GPR
-    var shopItemGPR: ShopItem = ShopItem.new()
-    shopItemGPR.id = 2;
-    shopItemGPR.maxLevel = 1
-    shopItemGPR.currentLevel = 0
-    shopItemGPR.price = [50]
-    shopItems.append(shopItemGPR) 
-    
-    #GPR width
-    var shopItemGPRWidth: ShopItem = ShopItem.new()
-    shopItemGPRWidth.id = 3;
-    shopItemGPRWidth.maxLevel = 3
-    shopItemGPRWidth.currentLevel = 0
-    shopItemGPRWidth.price = [125, 250, 500]
-    shopItems.append(shopItemGPRWidth) 
-    
-    #GPR depth
-    var shopItemGPRDepth: ShopItem = ShopItem.new()
-    shopItemGPRDepth.id = 4;
-    shopItemGPRDepth.maxLevel = 3
-    shopItemGPRDepth.currentLevel = 0
-    shopItemGPRDepth.price = [125, 250, 500]
-    shopItems.append(shopItemGPRDepth) 
-    
-    shop_has_changed.emit(shopItems)
 
 func collect_bone(bone_name: String):
     print("Bone collected: " + bone_name)
@@ -138,66 +50,10 @@ func setCollectionCompleted():
 func add_bone_damage(damage: BoneDamage):
     bone_damages.append(damage)
 
-func update_coins(amount: int):
-    self.coins += amount
-    coins_has_changed.emit(self.coins)
-
-func add_100_coin():
-    self.update_coins(100)
-func remove_100_coin():
-    self.update_coins(-100)
-
-func buy_item(shopItem: ShopItem):
+func update_tool_state(shopItems: Dictionary[String, Shop.ShopItem]):
     if Toolbelt.currentTool != Toolbelt.Tools.Hand:
         toolbelt.change_tool(Toolbelt.Tools.Hand)
-    print("Try buying Item!")
-    var price = shopItem.current_price()
-    if shopItem.can_be_bought(coins):
-        print("Bought Shovel!")
-        update_coins(-price)
-        shopItem.updateOne()
-        update_tool_state()
-        shop_has_changed.emit(shopItems)
-    else:
-        print("Cant buy Shovel!")
-
-func buy_shovel():
-    var shopItem = shopItems[0] as ShopItem
-    if shopItem:
-        self.buy_item(shopItem)
-    else:
-        print("No ShopItem!")
-
-func buy_trowel():
-    var shopItem = shopItems[1] as ShopItem
-    if shopItem:
-        self.buy_item(shopItem)
-    else:
-        print("No ShopItem!")
-
-func buy_gpr():
-    var shopItem = shopItems[2] as ShopItem
-    if shopItem:
-        self.buy_item(shopItem)
-    else:
-        print("No ShopItem!")
-        
-func buy_gpr_wight():
-    var shopItem = shopItems[3] as ShopItem
-    if shopItem:
-        self.buy_item(shopItem)
-    else:
-        print("No ShopItem!")
-
-func buy_gpr_depth():
-    var shopItem = shopItems[4] as ShopItem
-    if shopItem:
-        self.buy_item(shopItem)
-    else:
-        print("No ShopItem!")
-        
-func update_tool_state():
-    var shopItemShovel = shopItems[0] as ShopItem
+    var shopItemShovel = shopItems["shovel"] as Shop.ShopItem
     if shopItemShovel.currentLevel == 0:
         item_has_shovel = false
         item_shovel_big = false
@@ -208,7 +64,7 @@ func update_tool_state():
         item_has_shovel = true
         item_shovel_big = true
     
-    var shopItemTrowel = shopItems[1] as ShopItem
+    var shopItemTrowel = shopItems["trowel"] as Shop.ShopItem
     if shopItemTrowel.currentLevel == 0:
         item_has_trowel = false
         item_trowel_safe = false
@@ -219,16 +75,16 @@ func update_tool_state():
         item_has_trowel = true
         item_trowel_safe = true
     
-    var shopItemGpr = shopItems[2] as ShopItem
+    var shopItemGpr = shopItems["gpr"] as Shop.ShopItem
     if shopItemGpr.currentLevel == 0:
         item_has_gpr = false
     if shopItemGpr.currentLevel == 1:
         item_has_gpr = true
         
-    var shopItemGprW = shopItems[3] as ShopItem
+    var shopItemGprW = shopItems["gpr_width"] as Shop.ShopItem
     item_gpr_width = shopItemGprW.currentLevel
         
-    var shopItemGprD = shopItems[4] as ShopItem
+    var shopItemGprD = shopItems["gpr_depth"] as Shop.ShopItem
     item_gpr_depth = 3 + shopItemGprD.currentLevel
     
     
--- scripts/shop.gd
+++ scripts/shop.gd
@@ -1,36 +1,183 @@
 extends Node2D
 class_name Shop
 
-@export var labelPurse: Label
+@onready var labelPurse: Label = $ShopUI/Purse
+@onready var gamestate: GameState = $/root/MainScene/GameState
 
-func coins_has_changed(newValue: int) -> void:
-    labelPurse.text = "Coins: " + str(newValue)
+var shopItems: Dictionary[String, ShopItem]
+var coins: int
+     
+signal shop_has_changed(shopItems: Dictionary[String, ShopItem])
+signal coins_has_changed(newValue:int)
+signal coins_gained()
+signal coins_spent()
+signal insufficient_money()
 
-
-func _on_game_state_shop_has_changed(shopItems: Array[GameState.ShopItem]) -> void:
-    var shopItemShovel = shopItems[0] as GameState.ShopItem
-    if shopItemShovel.currentLevel == 0:
-        pass
-    elif shopItemShovel.currentLevel == 1:
-        pass
-    elif shopItemShovel.currentLevel == 2:
-        pass
-        
-    var shopItemTrowel = shopItems[1] as GameState.ShopItem
-    if shopItemTrowel.currentLevel == 0:
-        pass
-    elif shopItemTrowel.currentLevel == 1:
-        pass
-    elif shopItemTrowel.currentLevel == 2:
-        pass
+class ShopItem:
+    var id: int
+    var maxLevel: int
+    var currentLevel: int 
+    var price: Array[int]
     
-    var shopItemGpr = shopItems[2] as GameState.ShopItem
-    if shopItemGpr.currentLevel == 0:
-        pass
-    if shopItemGpr.currentLevel == 1:
-        pass
-        
-    var shopItemGprW = shopItems[3] as GameState.ShopItem
+    func isMaxLevel():
+        if self.currentLevel < self.maxLevel:
+            return false
+        return true
     
+    func current_price():
+        if self.currentLevel < price.size(): 
+            return price[self.currentLevel]
+        else:
+            return -1
+    
+    func updateOne():
+        if !self.isMaxLevel():
+            self.currentLevel += 1
+            return true
+        else:
+            return false
+            
+    func self_print():
+        print(
+            "Name: ", self.tool, " ", self.id,
+            " Level: ", self.currentLevel, "/", self.maxLevel,
+            " Prices: ", self.price,
+            )
+            
+    func can_be_bought(coins:int):
+        var currentPrice = self.current_price()
+        if !self.isMaxLevel() and currentPrice != -1 and currentPrice <= coins:
+            return true
+        return false
+            
+
+func update_coins(amount: int):
+    self.coins += amount
+    labelPurse.text = "$" + str(self.coins)
+    coins_has_changed.emit(self.coins)
+    if amount > 0:
+        coins_gained.emit()
+    elif amount < 0:
+        coins_spent.emit()
+
+func add_100_coin():
+    self.update_coins(100)
+func remove_100_coin():
+    self.update_coins(-100)
+
+func buy_item(shopItem: ShopItem):
+    print("Try buying Item!")
+    var price = shopItem.current_price()
+    if shopItem.can_be_bought(coins):
+        print("Bought Item!")
+        update_coins(-price)
+        shopItem.updateOne()
+        shop_has_changed.emit(shopItems)
+    else:
+        print("Cant buy Item!")
+        insufficient_money.emit()
+
+func buy_shovel():
+    var shopItem = shopItems["shovel"] as ShopItem
+    if shopItem:
+        self.buy_item(shopItem)
+    else:
+        print("No ShopItem!")
+
+func buy_trowel():
+    var shopItem = shopItems["trowel"] as ShopItem
+    if shopItem:
+        self.buy_item(shopItem)
+    else:
+        print("No ShopItem!")
+
+func buy_gpr():
+    var shopItem = shopItems["gpr"] as ShopItem
+    if shopItem:
+        self.buy_item(shopItem)
+    else:
+        print("No ShopItem!")
         
-    var shopItemGprD = shopItems[4] as GameState.ShopItem
+func buy_gpr_wight():
+    var shopItem = shopItems["gpr_width"] as ShopItem
+    if shopItem:
+        self.buy_item(shopItem)
+    else:
+        print("No ShopItem!")
+
+func buy_gpr_depth():
+    var shopItem = shopItems["gpr_depth"] as ShopItem
+    if shopItem:
+        self.buy_item(shopItem)
+    else:
+        print("No ShopItem!")
+        
+func _ready() -> void:
+    coins = 0
+    coins_has_changed.emit()
+    shopItems = {}
+    #Shovel 
+    var shopItemShovel: ShopItem = ShopItem.new()
+    shopItemShovel.id = 0;
+    shopItemShovel.maxLevel = 2
+    shopItemShovel.currentLevel = 0
+    shopItemShovel.price = [20, 250]
+    shopItems["shovel"] = shopItemShovel
+
+    #Trowel 
+    var shopItemTrowel: ShopItem = ShopItem.new()
+    shopItemTrowel.id = 1;
+    shopItemTrowel.maxLevel = 2
+    shopItemTrowel.currentLevel = 0
+    shopItemTrowel.price = [10, 400]
+    shopItems["trowel"] = shopItemTrowel 
+     
+    #GPR
+    var shopItemGPR: ShopItem = ShopItem.new()
+    shopItemGPR.id = 2;
+    shopItemGPR.maxLevel = 1
+    shopItemGPR.currentLevel = 0
+    shopItemGPR.price = [50]
+    shopItems["gpr"] = shopItemGPR 
+    
+    #GPR width
+    var shopItemGPRWidth: ShopItem = ShopItem.new()
+    shopItemGPRWidth.id = 3;
+    shopItemGPRWidth.maxLevel = 2
+    shopItemGPRWidth.currentLevel = 0
+    shopItemGPRWidth.price = [300, 1000]
+    shopItems["gpr_width"] = shopItemGPRWidth 
+    
+    #GPR depth
+    var shopItemGPRDepth: ShopItem = ShopItem.new()
+    shopItemGPRDepth.id = 4;
+    shopItemGPRDepth.maxLevel = 2
+    shopItemGPRDepth.currentLevel = 0
+    shopItemGPRDepth.price = [200, 750]
+    shopItems["gpr_depth"] = shopItemGPRDepth 
+    
+    shop_has_changed.connect(update_buttons)
+    shop_has_changed.emit(shopItems)
+    
+func update_buttons(_shopItems):
+    $ShopUI/Shovel.disabled = _shopItems["shovel"].currentLevel >= 1
+    
+    $ShopUI/Shovel2.visible = _shopItems["shovel"].currentLevel >= 1
+    $ShopUI/Shovel2.disabled = _shopItems["shovel"].currentLevel >= 2
+    
+    $ShopUI/Trowel.disabled = _shopItems["trowel"].currentLevel >= 1
+    
+    $ShopUI/Trowel2.visible =  _shopItems["trowel"].currentLevel >= 1
+    $ShopUI/Trowel2.disabled = _shopItems["trowel"].currentLevel >= 2
+    
+    $ShopUI/GPR.disabled = _shopItems["gpr"].currentLevel >= 1
+    
+    $ShopUI/GPRDepth.visible = _shopItems["gpr"].currentLevel >= 1
+    $ShopUI/GPRDepth.disabled = _shopItems["gpr_depth"].currentLevel != 0
+    $ShopUI/GPRDepth2.visible = _shopItems["gpr_depth"].currentLevel >= 1
+    $ShopUI/GPRDepth2.disabled = _shopItems["gpr_depth"].currentLevel != 1
+    
+    $ShopUI/GPRWidth.visible = _shopItems["gpr"].currentLevel >= 1
+    $ShopUI/GPRWidth.disabled = _shopItems["gpr_width"].currentLevel != 0
+    $ShopUI/GPRWidth2.visible = _shopItems["gpr_width"].currentLevel >= 1
+    $ShopUI/GPRWidth2.disabled = _shopItems["gpr_width"].currentLevel != 1
